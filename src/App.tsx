3935f838faad9265898d780294c8b081330f2333
Teddy <edmondteddy@hotmail.fr>
2023-03-12 13:19:04 +0100

    changing size values and working on changing the approch on the porblem

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import Shape from "./components/shape/shape"
+import Shape, { ShapeProps } from "./components/shape/shape"
+
+const colors = {
+  1: "#264653",
+  2: "#2A9D8F",
+  3: "#E9C46A",
+  4: "#F4A261",
+  5: "#E76F51",
+  6: "#EC8C74",
+  7: "#f5c4b7"
+}
+
+const shapesArray1: ShapeProps[] = [
+  { id: "1eds", color: colors[7], size: "small", radius: "none", top: "0%", left: "0%" },
+  { id: "2fd", color: colors[2], size: "small", radius: "none", top: "50%", left: "62%" },
+  { id: "3dze", color: colors[3], size: "medium", radius: "none", top: "50%", left: "0%" },
+  { id: "4ngn", color: colors[4], size: "medium", radius: "none", top: "0%", left: "38%" }
+]
+
+const shapesArray2: ShapeProps[] = [
+  { id: "5cxwc", color: colors[5], size: "extraSmall", radius: "none", top: "0%", left: "0%" },
+  { id: "6cvz", color: colors[6], size: "medium", radius: "none", top: "0%", left: "0%" },
+  { id: "7nvbf", color: colors[1], size: "large", radius: "none", top: "0%", left: "0%" }
+]
 
 function App() {
   return (
     <div className='App'>
-      <Shape color='limeGreen' size='small' radius='small' />
-      <Shape color='coral' size='large' radius='medium' />
+      <div className='box1'>
+        {shapesArray1.map((shape) => (
+          <Shape key={shape.id} {...shape} />
+        ))}
+      </div>
+      <div className='box2'>
+        {shapesArray2.map((shape) => (
+          <Shape key={shape.id} {...shape} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/shape/shape.tsx
+++ src/components/shape/shape.tsx
@@ -1,39 +1,45 @@
 import styled from "styled-components"
 
 const sizes = {
-  small: { width: "25%", height: "25%" },
-  medium: { width: "30%", height: "50%" },
-  long: { width: "15%", height: "80%" },
-  large: { width: "40%", height: "100%" }
+  extraSmall: { width: "37,5%", height: "25%" },
+  small: { width: "37,5%", height: "50%" },
+  medium: { width: "62,5%", height: "50%" },
+  large: { width: "100%", height: "100%" }
 }
 
 const borders = {
   none: "0px",
   small: "40px",
   medium: "100px",
-  large: "120px"
+  large: "100%"
 }
 
-interface ShapeProps {
+export interface ShapeProps {
+  id: string
   color: string
-  size: "small" | "medium" | "long" | "large"
+  size: "extraSmall" | "small" | "medium" | "large"
   radius: "none" | "small" | "medium" | "large"
+  top: string
+  left: string
 }
 
-const Div = styled.div`
+const Square = styled.div`
   background-color: ${(props) => props.color};
+  position: absolute;
 `
 
-export default function Shape({ color, size, radius }: ShapeProps) {
-  const style = {
+export default function Shape({ color, size, radius, top, left }: ShapeProps) {
+  const style: React.CSSProperties = {
     width: sizes[size].width,
     height: sizes[size].height,
-    borderRadius: borders[radius]
+    borderRadius: borders[radius],
+    top,
+    left
   }
 
   return (
     <>
-      <Div color={color} style={style} />
+      <Square color={color} style={style} />
     </>
   )
 }
